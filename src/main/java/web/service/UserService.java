459d1b0722f5ea960f071a3e00c293ab45246683
RadelNigma <radelnigma@yandex.ru>
2022-05-16 00:23:29 +0300

    Added Service interface

--- src/main/java/web/service/UserService.java
+++ src/main/java/web/service/UserService.java
@@ -1,35 +1,15 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import web.model.User;
-import web.repository.UserRepository;
 
 import java.util.List;
 
-@Service
-public class UserService {
+public interface UserService {
+    User findById(Long id);
 
-    private final UserRepository userRepository;
+    List<User> finedAll();
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    void saveUser(User user);
 
-    public User findById(Long id) {
-        return userRepository.getById(id);
-    }
-
-    public List<User> finedAll() {
-        return userRepository.findAll();
-    }
-
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public void deleteById(Long id) {
-        userRepository.deleteById(id);
-    }
+    void deleteById(Long id);
 }
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.model.User;
+import web.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.getById(id);
+    }
+
+    @Override
+    public List<User> finedAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
